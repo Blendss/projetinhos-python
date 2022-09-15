c4b5d5f067cb077dabff333d27e3e42a00e3a3c5
Thomas Buchser Monteiro <111438407+Blendss@users.noreply.github.com>
2022-09-15 08:40:08 -0300

    Add files via upload

---
+++ calcV2.py
@@ -0,0 +1,21 @@
+count = 1
+while(count == 1):
+    r3 = 0
+    r = 0
+    n1 = float(input('digite um valor: '))
+    sin = input('digite qual operaçao (+, -, *, /) quer realizar: ')
+    n2 = float(input('digite outro valor: '))
+    r = eval(f'{n1} {sin} {n2}')
+    print(r)
+    count = 2
+    while(count == 2):
+        sin2 = input('digite "C" para zerar ou qual operaçao (+, -, *, /) quer realizar: ')
+        if(sin2 == "C" or sin == "c"):
+            count = 1
+            print("\n\n")
+            break
+        n3 = float(input('digite um valor: '))
+        r2 = eval(f'{r} {sin2} {n3}')
+        print(r2)
+        r = r2
+print('acabou')
